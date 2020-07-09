75d0c97e6cefe91f34bcbcffcfea07454239100f
Armando Junio Rodrigues Montalvão <39529126+Armaanddo@users.noreply.github.com>
2020-07-09 19:59:21 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleBasicoDeEstoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco=0;
+        public int QtdeEstoque;
+
+       /* public void MostrarDadosDoProduto()
+        {
+            Console.WriteLine("Nome do produto: "+Nome);
+            Console.WriteLine("Preço:"+ Preco);
+            Console.WriteLine("Quantidade em estoque: "+QtdeEstoque);
+        }
+       */
+        public void AdicionarProdutos(int Qtde)
+        {
+            QtdeEstoque += Qtde;
+        }
+        public void RemoverProdutos(int Qtde)
+        {
+            QtdeEstoque -= Qtde;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return QtdeEstoque*Preco;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ControleBasicoDeEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            int qtde=0;
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados do produto: " + p.Nome + ", $ " + p.Preco + ", " + p.QtdeEstoque + " unidades, Total: $ " +p.ValorTotalEmEstoque());
+
+            Console.WriteLine("Digite o numero de produtos a ser adicionado ao estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+
+            Console.WriteLine("Dados atualizados: " + p.Nome + ", $ " + p.Preco + ", " + p.QtdeEstoque + " unidades, Total: $ " + p.ValorTotalEmEstoque());
+
+
+            Console.WriteLine("Digite o numero de produtos a ser removidos do estoque: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados atualizados: " + p.Nome + ", $ " + p.Preco + ", " + p.QtdeEstoque + " unidades, Total: $ " + p.ValorTotalEmEstoque());
+
+
+
+
+
+
+        }
+    }
+}
