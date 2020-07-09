1cb988acd7d4ff8fd4607eaf872bd38a41f57ab6
Armando Junio Rodrigues Montalvão <39529126+Armaanddo@users.noreply.github.com>
2020-07-09 11:56:52 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaSalarial
+{
+    class Funcionario
+    {
+        public double salario;
+        public string nome;
+    }
+}
